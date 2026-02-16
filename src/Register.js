84b7e38e7564d7b6272a4a3d29e338f908af8af6
mapalondeke1-cyber <mapalondeke1@gmail.com>
2026-02-16 23:09:30 +0200

    Update API endpoint to relative path

--- src/Register.js
+++ src/Register.js
@@ -19,7 +19,7 @@ const Register = () => {
 
     try {
       // 2. Send this package to our Backend Server on Port 5000
-      const response = await fetch('http://localhost:5000/api/register', {
+     const response = await fetch('/api/register', {
         method: 'POST',
         body: data,
       });
